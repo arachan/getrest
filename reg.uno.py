ac21bdc4b66a63d4deff02ae7e4930f64533fa85
Suzumizaki-Kimitaka <szmml@h12u.com>
2018-05-15 13:14:54 +0900

    Bring the file `getrest.py` to near the root

--- reg.uno.py
+++ reg.uno.py
@@ -4,11 +4,11 @@ import unohelper
 def createInstance( ctx ):
     # TokenCounter uses a new type, importing it at the top of this file
     # leads to a failure during adding the extension to OOo 
-    import org.openoffice.comp.addin.sample.python.getrest
-    return org.openoffice.comp.addin.sample.python.getrest.GetRest( ctx )
+    import com_hatenablog_arachansan.getrest
+    return com_hatenablog_arachansan.getrest.GetRest( ctx )
 
 # pythonloader looks for a static g_ImplementationHelper variable
 g_ImplementationHelper = unohelper.ImplementationHelper()
 g_ImplementationHelper.addImplementation( \
-	createInstance,"org.openoffice.comp.addin.sample.python.GetRest",
+	createInstance,"com_hatenablog_arachansan.GetRest",
         ("com.sun.star.sheet.AddIn",),)
