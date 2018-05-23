c926abd5cba216c91ef116f2373a9559343a4fb1
Yusuke arakawa <yusu.arakawa@gmail.com>
2018-05-23 15:03:27 +0900

    fixed: makeoxt

--- makeoxt.bat
+++ makeoxt.bat
@@ -1,11 +1,18 @@
 ::serch 7zip
+:: Scoop 7zip
 SET Z=%USERPROFILE%\scoop\shims\7z.exe
-
-::zip compression
 IF EXIST %Z% (GOTO SCOOP)
+:: Win x64 7zip x64
+:: Win x86 7xip x86
+SET Z="%ProgramFiles%\7-Zip\7z.exe"
+IF EXIST %Z% (GOTO 7Z)
+:: Win x64 7zip x86
+SET Z="%ProgramFiles(x86)%\7-Zip\7z.exe"
+IF EXIST %Z% (GOTO 7Z)
 
+::Compress
 :SCOOP
-ECHO "scoop is installed 7zip."
+ECHO "scoop installed 7zip."
 7z a -tzip getrest.zip reg.uno.py
 7z u getrest.zip META-INF
 7z u getrest.zip pythonpath
@@ -18,30 +25,8 @@ ECHO "scoop is installed 7zip."
 7z u getrest.zip makeoxt.bat
 GOTO BAT_END
 
-:: Windows10 x64 7zip x64
-:: Windows10 x86 7zip x86
-SET Z="%ProgramFiles%\7-Zip\7z.exe"
-IF EXIST %Z% (GOTO 7Z)
-
 :7Z
 ECHO "MSI Instelled 7zip"
-%Z% a -tzip getrest.zip reg.uno.py
-%Z% u getrest.zip META-INF
-%Z% u getrest.zip pythonpath
-%Z% u getrest.zip rdb
-%Z% u getrest.zip description.xml
-%Z% u getrest.zip LICENSE
-%Z% u getrest.zip mkoxt.sh
-%Z% u getrest.zip mkoxt.sh.conf
-%Z% u getrest.zip README.md
-%Z% u getrest.zip makeoxt.bat
-GOTO BAT_END
-
-:: Windows10 x64 7zip x86
-SET Z="%ProgramFiles(x86)%\7-Zip\7z.exe"
-IF EXIST %Z% (GOTO 7Z86)
-:7Z86
-ECHO "MSI Instelled 7zip x86"
 %Z% a -tzip getrest.zip reg.uno.py
 %Z% u getrest.zip META-INF
 %Z% u getrest.zip pythonpath
