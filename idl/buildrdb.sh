85b288f6c18ce0a68a9fbbdfa138b66dd0bf9424
Yusuke arakawa <yusu.arakawa@gmail.com>
2018-05-24 14:17:28 +0900

    modified:

--- idl/buildrdb.sh
+++ idl/buildrdb.sh
@@ -1,11 +1,25 @@
 # use this on linux with ooo build env
 
-# Debian/Ubuntu Package
+# Debian/Ubuntu package
+IDLC=/usr/lib/libreoffice/sdk/bin/idlc
+REGMERGE=/usr/lib/libreoffice/program/regmerge
 IDLPATH=/usr/lib/libreoffice/sdk/idl
-# Official rpm/deb package
-#IDLPATH=/opt/libreoffice6.0/sdk/idl
-# Redhat/CentOS Package
+
+# RedHat/CentOS x86_64
+# CentOS 7 (1804)
+#IDLC=/usr/lib64/libreoffice/sdk/bin/idlc
+#REGMERGE=/usr/lib64/libreoffice/program/regmerge
 #IDLPATH=/usr/lib64/libreoffice/sdk/idl
 
-idlc -I $IDLPATH XGetRest.idl
-regmerge ../rdb/getrest.rdb UCR  XGetRest.urd
+# macOS package
+#IDLC=/Users/arakawayusuke/LibreOffice6.0_SDK/bin/idlc
+#REGMERGE=/Applications/LibreOffice.app/Contents/MacOS/regmerge
+#IDLPATH=/Users/arakawayusuke/LibreOffice6.0_SDK/idl
+
+# LibreOffice Official (deb/rpm) package
+#IDLC=/opt/libreoffice6.0/sdk/bin/idlc
+#REGMERGE=/opt/libreoffice6.0/program/regmerge
+#IDLPATH=/opt/libreoffice6.0/sdk/idl
+
+$IDLC -I $IDLPATH XGetRest.idl
+$REGMERGE ../rdb/getrest.rdb UCR  XGetRest.urd
